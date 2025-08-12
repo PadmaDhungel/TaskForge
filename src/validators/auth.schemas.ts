8d38e503b2f84377642766794256d4d6da0e58ce
PadmaDhungel <padma.qcoder@gmail.com>
2025-08-12 22:59:25 +1000

    feat(routes): unify health check, add 404 handler, and integration tests

--- src/validators/auth.schemas.ts
+++ src/validators/auth.schemas.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-    email: z.string().email({ message: 'Invalid Email Format' }),
-    password: z.string()
+    email: z.string({ error: 'Email is required' }).email({ message: 'Invalid Email Format' }),
+    password: z.string({ error: 'password is required' })
         .nonempty({ message: 'Password is required' })  // <-- catch missing/empty password
         .min(8, { message: 'Password must be at least 8 characters long' }),
-    name: z.string()
+    name: z.string({ error: 'name is required' })
         .nonempty({ message: 'Name is required' }),
-}).strict();
+}).strip();//.strict works except for that it fails the extra field, os .strip is favoured
